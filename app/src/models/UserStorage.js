8fe402ddd8178043d561e681d596e33dc54a0e65
Ju Geonjeong <geun0j@gmail.com>
2021-10-08 11:00:33 +0900

    User 모델 구현

--- app/src/models/UserStorage.js
+++ app/src/models/UserStorage.js
@@ -6,15 +6,26 @@ class UserStorage {
         password: ["1234", "1234" ,"1234"],
         name: ["관리자1", "관리자2", "관리자3"],
     }
-    static getUsers(...fields){
+    static getUsers(...fields) {
         const users = this.#users;
         const newUsers = fields.reduce((newUsers, field) => {
-            if(users.hasOwnProperty(field)) {
+            if (users.hasOwnProperty(field)) {
                 newUsers[field] = users[field];
             }
             return newUsers;
         }, {});
         return newUsers;
+    }
+    static getUserInfo(id) {
+        const users = this.#users;
+        const idx = users.id.indexOf(id);
+        const usersKeys = Object.keys(users);
+        const userInfo = usersKeys.reduce((newUser, info) => {
+            newUser[info] = users[info][idx];
+            return newUser;
+        }, {})
+
+        return userInfo;
     }
 }
 
